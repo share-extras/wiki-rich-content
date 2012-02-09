32fac8962aaee256b8d460f914f53e9ec5391e5b
Will Abson <will.abson@gmail.com>
2012-02-09 13:29:37 +0000

    Wiki rich content document previews now converted to use a dedicated page definition to fetch the pre-generated HTML for doc-previews, avoids having to build this using the Dom and manually create a WebPreview object instance.
    git-svn-id: https://share-extras.googlecode.com/svn/trunk/Wiki Rich Content@962 a3f5c567-fd0f-3a89-9b71-a290c5a5f590

--- source/web/extras/components/wiki/document-parser.js
+++ source/web/extras/components/wiki/document-parser.js
@@ -23,9 +23,7 @@
    /**
     * YUI Library aliases
     */
-   var Dom = YAHOO.util.Dom,
-       Event = YAHOO.util.Event,
-       Element = YAHOO.util.Element;
+   var Dom = YAHOO.util.Dom;
    
    /**
     * WikiDocumentParser constructor.
@@ -67,9 +65,7 @@
           * @type String
           * @default "embednolink"
           */
-         embedTargetNoLink: "embednolink",
-         
-         pluginConditions: [{"attributes": {"thumbnail": "imgpreview", "mimeType": "video\/mp4"}, "plugins": [{"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "StrobeMediaPlayback"}, {"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "FlashFox"}, {"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "Video"}]}, {"attributes": {"thumbnail": "imgpreview", "mimeType": "video\/m4v"}, "plugins": [{"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "StrobeMediaPlayback"}, {"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "FlashFox"}, {"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "Video"}]}, {"attributes": {"thumbnail": "imgpreview", "mimeType": "video\/x-flv"}, "plugins": [{"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "StrobeMediaPlayback"}, {"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "FlashFox"}]}, {"attributes": {"thumbnail": "imgpreview", "mimeType": "video\/quicktime"}, "plugins": [{"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "StrobeMediaPlayback"}]}, {"attributes": {"thumbnail": "imgpreview", "mimeType": "video\/ogg"}, "plugins": [{"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "Video"}]}, {"attributes": {"thumbnail": "imgpreview", "mimeType": "video\/webm"}, "plugins": [{"attributes": {"poster": "imgpreview", "posterFileSuffix": ".png"}, "name": "Video"}]}, {"attributes": {"mimeType": "video\/mp4"}, "plugins": [{"attributes": {}, "name": "StrobeMediaPlayback"}, {"attributes": {}, "name": "FlashFox"}, {"attributes": {}, "name": "Video"}]}, {"attributes": {"mimeType": "video\/x-m4v"}, "plugins": [{"attributes": {}, "name": "StrobeMediaPlayback"}, {"attributes": {}, "name": "FlashFox"}, {"attributes": {}, "name": "Video"}]}, {"attributes": {"mimeType": "video\/x-flv"}, "plugins": [{"attributes": {}, "name": "StrobeMediaPlayback"}, {"attributes": {}, "name": "FlashFox"}]}, {"attributes": {"mimeType": "video\/quicktime"}, "plugins": [{"attributes": {}, "name": "StrobeMediaPlayback"}]}, {"attributes": {"mimeType": "video\/ogg"}, "plugins": [{"attributes": {}, "name": "Video"}]}, {"attributes": {"mimeType": "video\/webm"}, "plugins": [{"attributes": {}, "name": "Video"}]}, {"attributes": {"mimeType": "audio\/mpeg"}, "plugins": [{"attributes": {}, "name": "StrobeMediaPlayback"}, {"attributes": {}, "name": "FlashFox"}, {"attributes": {}, "name": "Audio"}]}, {"attributes": {"mimeType": "audio\/x-wav"}, "plugins": [{"attributes": {}, "name": "Audio"}]}, {"attributes": {"thumbnail": "webpreview"}, "plugins": [{"attributes": {"paging": "true", "src": "webpreview"}, "name": "WebPreviewer"}]}, {"attributes": {"thumbnail": "imgpreview"}, "plugins": [{"attributes": {"src": "imgpreview"}, "name": "Image"}]}, {"attributes": {"mimeType": "image\/jpeg"}, "plugins": [{"attributes": {"srcMaxSize": "500000"}, "name": "Image"}]}, {"attributes": {"mimeType": "image\/png"}, "plugins": [{"attributes": {"srcMaxSize": "500000"}, "name": "Image"}]}, {"attributes": {"mimeType": "image\/gif"}, "plugins": [{"attributes": {"srcMaxSize": "500000"}, "name": "Image"}]}, {"attributes": {"mimeType": "application\/x-shockwave-flash"}, "plugins": [{"attributes": {}, "name": "Flash"}]}]
+         embedTargetNoLink: "embednolink"
       },
       
       /**
@@ -84,7 +80,7 @@
          var pageObj = args[1].pageObj, 
             textEl = args[1].textEl, 
             linkEls = textEl.getElementsByTagName("a"), 
-            linkEl, link, embedUrl, embed, embedContainer,
+            linkEl, link, embed, embedContainer,
             includeLink,
             docRe = new RegExp("\\/document-details\\/?\\?nodeRef=(\\w+:\\/\\/\\w+\\/[-\\w]+)"),
             docMatch, nodeRef;
@@ -104,73 +100,40 @@
                   
                   // 4.0 style - much simpler
                   var previewEl = document.createElement("DIV"), // container element
-                     bdEl = document.createElement("DIV"), // body element
-                     pEl = document.createElement("DIV"), // previewer element
-                     msgEl = document.createElement("DIV"), // message element
                      elId = Dom.generateId(previewEl, "preview-");
 
-                  msgEl.innerHTML = "Preparing preview..."; // optional
-                  Dom.addClass(msgEl, "message"); // optional
-                  pEl.appendChild(msgEl); // optional
-                  Dom.setAttribute(pEl, "id", elId + "-previewer-div");
-                  Dom.addClass(pEl, "previewer");
-                  bdEl.appendChild(pEl);
-                  Dom.setAttribute(bdEl, "id", elId + "-body");
-                  Dom.addClass(bdEl, "web-preview");
-                  previewEl.appendChild(bdEl);
-
-                  // Load the list of columns for this data type
-                  Alfresco.util.Ajax.jsonGet(
+                  // Load the web-previewer mark-up using the custom page definition, which just includes that component
+                  Alfresco.util.Ajax.request(
                   {
-                     url: Alfresco.constants.PROXY_URI + "slingshot/doclib/node/" + nodeRef.replace("://", "/"),
+                     url: Alfresco.constants.URL_PAGECONTEXT + "site/" + pageObj.options.siteId + "/wiki-document-preview",
+                     dataObj: {
+                        nodeRef: nodeRef
+                     },
                      successCallback:
                      {
                         fn: function WikiDocumentParser__createFromHTML_success(p_response, p_obj)
                         {
-                           var nodeRef = p_response.json.item.nodeRef,
-                              fileName = p_response.json.item.fileName,
-                              mimetype = p_response.json.item.mimetype,
-                              size = p_response.json.item.size;
-                           
-                           // TODO Define some global messages
-                           /*
-                            * The web preview component can't retrieve the list of previews itself,
-                            * so we need to do this for it.
-                            */
-                            Alfresco.util.Ajax.jsonGet(
-                            {
-                               url: Alfresco.constants.PROXY_URI + "api/node/" + nodeRef.replace(":/", "") + "/content/thumbnaildefinitions",
-                               successCallback:
-                               {
-                                  fn: function WDP_onLoadThumbnailDefinitions(p_thmbResp, p_obj)
-                                  {
-                                      var previews = p_thmbResp.json;
-                                      Dom.addClass(p_response.config.object.previewEl, "wiki-doc-preview");
-                                      new Alfresco.WebPreview(p_response.config.object.elId).setOptions(
-                                      {
-                                         nodeRef: nodeRef,
-                                         name: fileName,
-                                         mimeType: mimetype,
-                                         size: size,
-                                         thumbnails: previews,
-                                         pluginConditions: this.options.pluginConditions
-                                      }).setMessages(
-                                            Alfresco.messages.scope["Extras.WikiPageParsers"]
-                                      );
-                                  },
-                                  scope: this
-                               },
-                               failureMessage: "Could not load thumbnail definitions list"
-                           });
-                           
-                           /*
-                           YAHOO.Bubbling.fire("documentDetailsAvailable", {
-                              documentDetails: p_response.json.item
-                           });
-                           */
+                           var phtml = p_response.serverResponse.responseText.replace(/template_x002e_web-preview/g, p_response.config.object.elId),
+                              result = Alfresco.util.Ajax.sanitizeMarkup(phtml);
+                           // Following code borrowed from Alfresco.util.Ajax._successHandler
+                           // Use setTimeout to execute the script. Note scope will always be "window"
+                           var scripts = result[1];
+                           if (YAHOO.lang.trim(scripts).length > 0)
+                           {
+                              window.setTimeout(scripts, 0);
+                              // Delay-call the PostExec function to continue response processing after the setTimeout above
+                              YAHOO.lang.later(0, this, function() {
+                                 Alfresco.util.YUILoaderHelper.loadComponents();
+                              }, p_response.serverResponse);
+                           }
+                           p_response.config.object.previewEl.innerHTML = result[0];
+                           Dom.addClass(p_response.config.object.previewEl, "wiki-doc-preview");
                         },
                         scope: this
                      },
+                     // Unfortunately we cannot set execScripts to true, as we need to first update the element ids in the html to make them unique, before the scripts are run
+                     // So instead we execute the scripts manually, above
+                     //execScripts: true,
                      failureMessage: "Failed to load document details for " + nodeRef,
                      scope: this,
                      object: {
